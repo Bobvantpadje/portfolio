79f5b103f711380e7aca33ddfeab4c8241fd8878
bobvantpadje <bobvantpadje@gmail.com>
2020-10-27 11:25:49 +0100

    created skills constant

---
+++ src/utils/contants/skills.ts
@@ -0,0 +1,13 @@
+type skill = {
+  name: string;
+  imagePath: string;
+  color: string;
+  percentage: number;
+};
+
+export const skills: skill[] = [
+  { name: 'Javascript', imagePath: 'images/skills/js', color: 'yellow', percentage: 50 },
+  { name: 'Javascript', imagePath: 'images/skills/js', color: 'yellow', percentage: 50 },
+  { name: 'Javascript', imagePath: 'images/skills/js', color: 'yellow', percentage: 50 },
+  { name: 'Javascript', imagePath: 'images/skills/js', color: 'yellow', percentage: 50 }
+];
