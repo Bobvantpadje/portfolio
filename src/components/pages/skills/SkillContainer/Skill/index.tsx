b7630b0caf5fc1774daa7205b11bda7c3458f206
bobvantpadje <bobvantpadje@gmail.com>
2020-10-28 16:51:47 +0100

    created svg with circle instead of path

--- src/components/pages/skills/SkillContainer/Skill/index.tsx
+++ src/components/pages/skills/SkillContainer/Skill/index.tsx
@@ -47,36 +47,71 @@ const Title: FC<{ skill: Skill }> = ({ skill }) => {
   );
 };
 
+const circleRadius = 52;
+const size = 60;
+const dashPath = 2 * Math.PI * circleRadius;
 const ProgressChart: FC<{ percentage: number; color: string; delay?: number }> = ({ percentage, color, delay = 0 }) => {
-  const pathLength = useSpring(100);
-  const zero = useSpring(0);
+  const progressCircle = dashPath * (1 - percentage);
   return (
-    <svg viewBox="0 0 60 60">
-      <motion.path
-        fill="none"
-        strokeWidth="7"
-        stroke={addOpacity(color, 0.5)}
-        d="M 0, 20 a 20, 20 0 1,0 40,0 a 20, 20 0 1,0 -40,0"
-        strokeDasharray="0 1"
-        initial={{ pathLength: 1, rotate: 90, translateX: 10, translateY: 15 }}
-        // animate={{ pathLength: 1 }}
-        // transition={{ delay: 1 }}
-      />
-      <motion.path
-        fill="none"
-        strokeWidth="7"
-        stroke={addOpacity(color, 0.75)}
-        d="M 0, 20 a 20, 20 0 1,0 40,0 a 20, 20 0 1,0 -40,0"
-        strokeDasharray="0 1"
-        variants={container}
-        initial="hidden"
-        animate="show"
-        transition={{ duration: 1, delay: delay + 1 }}
-        // initial={{ pathLength: 0, rotate: 90, translateX: 10, translateY: 15 }}
-        // animate={{ pathLength: 0.9 }}
-        // transition={{ delay: 1, duration: 1 }}
-      />
-    </svg>
+    <Box position="relative">
+      <svg width={size * 2} height={size * 2} viewBox={`0 0 ${size * 2} ${size * 2}`}>
+        <motion.circle
+          cx={size}
+          cy={size}
+          r={circleRadius}
+          strokeWidth={(size - circleRadius) * 2}
+          fill="none"
+          stroke={addOpacity(color, 0.5)}
+          initial={{ pathLength: 0.5, rotate: -90, strokeDashoffset: 0, strokeDasharray: dashPath, scaleX: -1, scaleY: 1 }}
+        />
+        <motion.circle
+          cx={size}
+          cy={size}
+          r={circleRadius}
+          strokeWidth={(size - circleRadius) * 2}
+          fill="none"
+          stroke={addOpacity(color, 0.75)}
+          initial={{ pathLength: 0.5, rotate: -90, strokeDashoffset: progressCircle, strokeDasharray: dashPath, scaleX: -1, scaleY: 1 }}
+        />
+        {/* <motion.circle
+          cx="60"
+          cy="60"
+          r="54"
+          strokeWidth="12"
+          fill="none"
+          stroke={addOpacity(color, 0.75)}
+          initial="hidden"
+          animate="show"
+          transition={{ duration: 1, delay: delay + 1 }}
+        /> */}
+        {/* <circle cx="60" cy="60" r="54" stroke-width="12" /> */}
+      </svg>
+      {/* <svg viewBox="0 0 60 60"> */}
+      {/* <motion.path
+          fill="none"
+          strokeWidth="7"
+          stroke={addOpacity(color, 0.5)}
+          d="M 0, 30 a 30, 30 0 1,0 40,0 a 30, 30 0 1,0 -60,0"
+          strokeDasharray="0 1"
+          // initial={{ pathLength: 1, rotate: 90, translateX: 10, translateY: 15 }}
+          initial={{ pathLength: 1, rotate: 90, translateX: 10 }}
+        /> */}
+      {/* <motion.path
+          fill="none"
+          strokeWidth="7"
+          stroke={addOpacity(color, 0.75)}
+          d="M 0, 20 a 20, 20 0 1,0 40,0 a 20, 20 0 1,0 -40,0"
+          strokeDasharray="0 1"
+          variants={container}
+          initial="hidden"
+          animate="show"
+          transition={{ duration: 1, delay: delay + 1 }}
+        /> */}
+      {/* </svg> */}
+      <Box position="absolute" left={0} right={0} ml="auto" mr="auto" top="50%">
+        {percentage}
+      </Box>
+    </Box>
   );
 };
 
@@ -85,14 +120,14 @@ const container = {
     // transition: { delay: 1, duration: 1 },
     pathLength: 0.9,
     rotate: 90,
-    translateX: 10,
-    translateY: 15
+    translateX: 10
+    // translateY: 15
   },
   hidden: {
     // transition: { delay: 1, duration: 1 },
     pathLength: 0,
     rotate: 90,
-    translateX: 10,
-    translateY: 15
+    translateX: 10
+    // translateY: 15
   }
 };
--- src/utils/contants/skills.ts
+++ src/utils/contants/skills.ts
@@ -7,11 +7,11 @@ type skill = {
 
 // Color should always to rb
 export const skills: skill[] = [
+  { name: 'Javascript', imagePath: 'images/skills/javascript-icon.png', color: 'rgba(246, 218, 75)', percentage: 0.1 },
+  { name: 'Javascript', imagePath: 'images/skills/javascript-icon.png', color: 'rgba(246, 218, 75)', percentage: 0.2 },
+  { name: 'Javascript', imagePath: 'images/skills/javascript-icon.png', color: 'rgba(246, 218, 75)', percentage: 0.3 },
+  { name: 'Javascript', imagePath: 'images/skills/javascript-icon.png', color: 'rgba(246, 218, 75)', percentage: 0.4 },
   { name: 'Javascript', imagePath: 'images/skills/javascript-icon.png', color: 'rgba(246, 218, 75)', percentage: 0.5 },
-  { name: 'Javascript', imagePath: 'images/skills/javascript-icon.png', color: 'rgba(246, 218, 75)', percentage: 0.5 },
-  { name: 'Javascript', imagePath: 'images/skills/javascript-icon.png', color: 'rgba(246, 218, 75)', percentage: 0.5 },
-  { name: 'Javascript', imagePath: 'images/skills/javascript-icon.png', color: 'rgba(246, 218, 75)', percentage: 0.5 },
-  { name: 'Javascript', imagePath: 'images/skills/javascript-icon.png', color: 'rgba(246, 218, 75)', percentage: 0.5 },
-  { name: 'Javascript', imagePath: 'images/skills/javascript-icon.png', color: 'rgba(246, 218, 75)', percentage: 0.5 },
-  { name: 'Javascript', imagePath: 'images/skills/javascript-icon.png', color: 'rgba(246, 218, 75)', percentage: 0.5 }
+  { name: 'Javascript', imagePath: 'images/skills/javascript-icon.png', color: 'rgba(246, 218, 75)', percentage: 0.6 },
+  { name: 'Javascript', imagePath: 'images/skills/javascript-icon.png', color: 'rgba(246, 218, 75)', percentage: 0.7 }
 ];
