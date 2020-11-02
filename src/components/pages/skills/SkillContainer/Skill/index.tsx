70bd33cba624f0e5c42b8741df9e098d26e26bb4
bobvantpadje <bobvantpadje@gmail.com>
2020-11-02 16:25:02 +0100

    change size of chart dynamically

--- src/components/pages/skills/SkillContainer/Skill/index.tsx
+++ src/components/pages/skills/SkillContainer/Skill/index.tsx
@@ -1,12 +1,9 @@
 import { ProgressChart } from 'components/utils/ProgressChart';
-import { motion, useSpring, animate } from 'framer-motion';
-import React, { FC, useEffect, useRef } from 'react';
-// import { animate } from 'framer-motion';
-import { opacity } from 'styled-system';
+import React, { FC } from 'react';
 import { MotionBox } from 'styles/StyledComponents/Animated/MotionBox';
 import { Box } from 'styles/StyledComponents/Box';
-import { Paragraph } from 'styles/StyledComponents/Text/Paragraph';
 import { addOpacity } from 'utils/helpers/addOpacity';
+import { useWindowSize } from 'utils/hooks/useWindowSize';
 import { SkillTitle } from './SkillTitle';
 
 const variants = {
@@ -21,6 +18,9 @@ const variants = {
 };
 
 export const Skill: FC<{ skill: Skill; delay: number }> = ({ skill, delay }) => {
+  const windowSize = useWindowSize();
+  const chartSize = getChartSize({ width: windowSize.width });
+
   return (
     <MotionBox
       display="flex"
@@ -32,8 +32,13 @@ export const Skill: FC<{ skill: Skill; delay: number }> = ({ skill, delay }) => 
       p={[3]}>
       <SkillTitle skill={skill} />
       <Box mt={[0, 3]} ml={['auto', 0]} position="relative" top={[-14, 0]}>
-        <ProgressChart percentage={skill.percentage} color={skill.color} delay={delay} />
+        <ProgressChart percentage={skill.percentage} color={skill.color} delay={delay} size={chartSize} />
       </Box>
     </MotionBox>
   );
+};
+
+const getChartSize = ({ width }: { width: number }) => {
+  if (width < 768) return 30;
+  return 60;
 };
