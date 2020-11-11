a72a6be673f0d53e6803928d5b9e93742015c337
bobvantpadje <bobvantpadje@gmail.com>
2020-11-11 08:47:56 +0100

    load skill icon dynamically from the images folder instead of hardcoded value is skillTitle component

--- src/components/pages/skills/SkillContainer/Skill/SkillTitle.tsx
+++ src/components/pages/skills/SkillContainer/Skill/SkillTitle.tsx
@@ -13,7 +13,7 @@ export const SkillTitle: FC<{ skill: Skill }> = ({ skill }) => {
         justifyContent="center"
         alignItems="center"
         borderRadius={[1]}>
-        <img src={'images/skills/javascript-icon.png'} height="60%" />
+        <img src={skill.imagePath} height="60%" />
       </Box>
       <Box ml={[2]}>{skill.name}</Box>
     </Box>
